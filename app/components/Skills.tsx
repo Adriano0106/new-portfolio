db4a918bb80c5ebc8ce81e78dca3d38bfafd454e
Adriano Andrade <adriano.andrade.silva@hotmail.com>
2024-07-15 19:40:31 -0300

    Ajusta tecnologias para exibir em lista e trazendo a imagem

--- app/components/Skills.tsx
+++ app/components/Skills.tsx
@@ -1,23 +1,26 @@
+import Image from "next/image";
+
 const my_skills = [
-  ["JavaScript", 95],
-  ["TypeScript", 70],
-  ["ReactJS", 80],
-  ["NextJS", 70],
-  ["NodeJS", 70],
-  ["Ruby", 75],
-  ["Ruby on Rails", 70],
-  ["Git", 75],
-  ["Bootstrap", 80],
-  ["Sass (SCSS)", 80],
-  ["CSS", 100],
-  ["HTML", 100],
+  ["JavaScript", "/techs/js.png"],
+  ["TypeScript", "/techs/ts.png"],
+  ["ReactJS", "/techs/react.png"],
+  ["NextJS", "/techs/next.png"],
+  ["NodeJS", "/techs/node.png"],
+  ["Ruby", "/techs/ruby.png"],
+  ["Ruby on Rails", "/techs/rails.png"],
+  ["Git", "/techs/git.png"],
+  ["Bootstrap", "/techs/bootstrap.png"],
+  ["Sass (SCSS)", "/techs/sass.png"],
+  ["CSS", "/techs/css.png"],
 ]
 
 const Skills = () => {
   return (
     <section id="skills-section" style={{ height: "800px", width: "inherit" }} className="px-2 sm:px-6 lg:px-8 scroll-pt-navbar pt-16">
       <h1>Skills</h1>
-      <div>
+
+
+      {/* <div>
       {my_skills.map((skill) => (
         <div key={skill[0].toString().toLocaleLowerCase()}>
           <div className="mb-1 text-base font-medium text-gray-700 dark:text-gray-500">{skill[0]}</div>
@@ -26,7 +29,25 @@ const Skills = () => {
           </div>
         </div>
       ))}
-      </div>
+      </div> */}
+
+      <ul className="space-y-4 text-left text-gray-500 dark:text-gray-400">
+        {my_skills.map((skill, index) => (
+          <li key={index} className="flex items-center space-x-3 rtl:space-x-reverse">
+            <span>
+              <Image
+                src={skill[1]}
+                width={20}
+                height={20}
+                alt="Picture tecnology"
+                className="skills__picture"
+              // style={{ borderRadius: "50%" }}
+              />
+            </span>
+            <span>{skill[0]}</span>
+          </li>
+        ))}
+      </ul>
     </section>
   )
 }
