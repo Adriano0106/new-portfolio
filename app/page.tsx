e5afcc2c36de2b2e0645e375a27e6568c3eac260
Adriano Andrade <adriano.andrade.silva@hotmail.com>
2024-07-15 18:53:39 -0300

    Adiciona classe de width 100% no grid de Skills e Hobbirs

--- app/page.tsx
+++ app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <div className='mx-auto max-w-7xl px-2 sm:px-6 lg:px-8'>
       <Presentation />
       <Experience />
-      <div className="flex flex-col-reverse md:flex-row">
+      <div className="w-full flex flex-col-reverse md:flex-row">
         <Skills />
         <Hobbies />
       </div>
